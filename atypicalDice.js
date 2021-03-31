0fa45c12368b73b83617d0630ccb64854b70b490
Dominic Tarro <57306102+dominictarro@users.noreply.github.com>
2021-03-31 15:45:25 -0400

    Added problem description to comments header

--- atypicalDice.js
+++ atypicalDice.js
@@ -1,3 +1,11 @@
+/*
+** Problem **
+Patrick has nine, four-sided dice (pyramidal dice) with faces labeled 1 through 4. Claire has six, six-sided
+dice (cubic dice) with faces labeled 1 through 9. Patrick and Claire roll their dice and compare the sum of
+the faces: the person with the highest total wins. What is the probability Pyramidal Patrick beats Cubic
+Claire? Round to seven decimal places.
+NOTE: Experiencing a rounding error to 5 decimals.
+*/
 
 function nCr(n, r) {
     /*
